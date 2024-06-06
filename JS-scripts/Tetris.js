faa11961d850b1336dfee9bec425f039dfcc6849
etherusko <etherusko.labs@gmail.com>
2024-06-06 01:32:57 -0600

    first tetris sketch without rotation

---
+++ JS-scripts/Tetris.js
@@ -0,0 +1,80 @@
+export class Tetris{
+    static Shapes = [
+                    [[1,1],
+                     [1,1]
+                    ],
+                    [[1,1,0],
+                     [0,1,1]
+                    ],
+                    [[0,1,1],
+                     [1,1,0]
+                    ],
+                    [[0,1,0],
+                     [1,1,1]
+                    ],
+                    [[1,1,1,1]
+                    ],
+                    [[1,0],
+                     [1,1]
+                    ],
+                    [[1]
+                    ]
+                    ]
+    #Rows = 20;
+    #Cols = 10;
+    #BlockSize = 25;
+    constructor(context){
+        this.width = this.#Cols*this.#BlockSize
+        this.height = this.#Rows*this.#BlockSize
+        this.Board = this.#makeBoard();
+        this.cxt = context;
+        this.shape = Tetris.Shapes[Math.floor(Math.random()*7)];
+        this.posx = 4;
+        this.posy = 0;
+    }
+
+    #makeBoard(){
+        let arr = [];
+        for(let i=0;i<this.#Rows;i++){
+            arr[i]=[0,0,0,0,0,0,0,0,0,0];
+        }
+        arr.push([1,1,1,1,1,1,1,1,1,1]);
+        return arr;
+     }
+     drawBoard(){
+        this.cxt.fillStyle = 'yellow';
+        this.Board.forEach((row,y) => row.forEach((e,x) =>{
+            if(e == 1) this.cxt.fillRect(x*25,y*25,25,25);
+        }))
+     }
+     drawShape(){   
+        this.cxt.fillStyle = 'yellow';
+        this.cxt.scale(this.#BlockSize,this.#BlockSize);
+        this.shape.forEach((row,y) => row.forEach((e,x) => {
+            if(e == 1) this.cxt.fillRect(this.posx+x,this.posy+y,1,1);
+        }))
+        this.cxt.scale(1/this.#BlockSize,1/this.#BlockSize);
+     }
+     drawGrid(){
+        this.cxt.lineWidth = 0.5;
+        this.cxt.strokeStyle = '#656548'
+        this.Board.forEach((row,y) => row.forEach((_,x) =>{
+            this.cxt.strokeRect(x*25,y*25,25,25);
+        }))
+     }
+     solidShape(){
+       this.shape.forEach((row,y)=>{
+            row.forEach((col,x)=>{if(col==1)this.Board[y+this.posy][x+this.posx]=1});
+            if(this.Board[y+this.posy].every(x => x==1)){
+                this.Board.splice(y+this.posy,1);
+                this.Board.unshift([0,0,0,0,0,0,0,0,0,0]);
+            }
+       });
+       this.shape = Tetris.Shapes[Math.floor(Math.random()*7)];
+       this.posy=0;
+       this.posx=4;
+     }
+     rotateShape(){
+        console.log('rotate');
+     }
+}
---
+++ JS-scripts/events.js
@@ -0,0 +1,33 @@
+import { Tetris } from "./Tetris.js";
+import { tetris } from "./main.js";
+//Eventos
+export const events = document.addEventListener('keydown',(e)=>{
+    if(e.key == 'ArrowRight') evalRightColitions();
+    if(e.key == 'ArrowLeft') evalLeftColitions();
+    if(e.key == 'ArrowDown') evalDownColitions();
+    if(e.key == 'c' || e.key == 'ArrowUp') tetris.rotateShape();  
+})
+
+function evalDownColitions(){
+    let boolean = true;
+    tetris.shape.forEach((row,y) => row.forEach((e,x) =>{
+        if(tetris.Board[y+tetris.posy+1][x+tetris.posx]==1 & e==1) boolean = false;
+    }))
+    boolean ? tetris.posy++ : tetris.solidShape();
+}
+function evalLeftColitions(){
+    let boolean = true;
+    tetris.shape.forEach((row,y) => row.forEach((e,x) =>{
+        if(tetris.posx==0) boolean = false;
+        if(tetris.Board[y+tetris.posy][x+tetris.posx-1]==1 & e==1) boolean = false;
+    }))
+    if(boolean) tetris.posx--;
+}
+function evalRightColitions(){
+    let boolean = true;
+    tetris.shape.forEach((row,y) => row.forEach((e,x) =>{
+        if(tetris.posx+tetris.shape[0].length>9) boolean = false;
+        if(tetris.Board[y+tetris.posy][x+tetris.posx+1]==1 & e==1) boolean = false;
+    }))
+    if(boolean) tetris.posx++;
+}
